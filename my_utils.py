23c46d652560c9e0a2e36418bfe07ecf809016c2
Alaina Bartfeld <alainabartfeld@gmail.com>
2025-12-25 21:08:08 -0500

    compartamentalize btwn limited and full listening history, also add capability to analyze more than 1 persons data file

--- my_utils.py
+++ my_utils.py
@@ -1,7 +1,14 @@
 #%%
 import logging
 import json
+import os
+from pathlib import Path
 import pandas as pd
+import duckdb
+
+#%%
+ms_to_mins = 60000
+mins_to_hrs = 60
 
 #%%
 def setup_logging(log_file_path):
@@ -12,11 +19,11 @@ def setup_logging(log_file_path):
         format="%(asctime)s - %(levelname)s - %(message)s"
     )
     
-def get_csvs(DATA_DIR, OUTPUT_DIR, csv_subpath):
+def get_csvs(DATA_DIR, OUTPUT_DIR, csv_subpath, pattern):
     # Find matching files
     logging.info("Grabbing streaming history")
 
-    pattern = "StreamingHistory_music*.json"
+    pattern = pattern
     json_files = DATA_DIR.glob(pattern)
     json_file_count = sum(1 for _ in DATA_DIR.glob(pattern))
 
@@ -37,7 +44,10 @@ def get_csvs(DATA_DIR, OUTPUT_DIR, csv_subpath):
     # returns nothing, just overwrites the JSON files as CSV's
 
 
-def make_combined_csv(OUTPUT_DIR, combined_file_name):
+def make_combined_csv(OUTPUT_DIR, combined_file_name, end_time_column, pattern):
+    # pattern of individual files (not the combined one) to delete at the end
+    pattern = pattern
+    
     # Output file
     OUTPUT_FILE = OUTPUT_DIR / combined_file_name
 
@@ -56,16 +66,47 @@ def make_combined_csv(OUTPUT_DIR, combined_file_name):
         ignore_index=True
     )
 
-    # Parse and sort by endTime
-    df["endTime"] = pd.to_datetime(df["endTime"], errors="coerce")
-    df = df.sort_values("endTime").reset_index(drop=True)
+    # Parse and sort by end_time_column
+    df[end_time_column] = pd.to_datetime(df[end_time_column], errors="coerce")
+    df = df.sort_values(end_time_column).reset_index(drop=True)
 
     # Write combined CSV
     df.to_csv(OUTPUT_FILE, index=False)
 
-    # Delete individual CSVs
-    for f in csv_files:
+    for f in OUTPUT_DIR.rglob(pattern):
+        logging.info(f"Deleting individual files")
         f.unlink()
 
-    logging.info(f"Created {OUTPUT_DIR}\{OUTPUT_FILE.name}, sorted by endTime, and deleted {len(csv_files)} CSV files.")
+    logging.info(f"Created {OUTPUT_DIR}\{OUTPUT_FILE.name}, sorted by {end_time_column}, and deleted {len(csv_files)} CSV files.")
     # returns nothing, just writes the combined CSV
+
+def init_paths(json_subpath, csv_subpath):
+    DATA_DIR = Path(os.path.join(os.path.dirname(__file__), json_subpath)) 
+    OUTPUT_DIR = Path(os.path.join(os.path.dirname(__file__), csv_subpath))
+    OUTPUT_DIR.mkdir(exist_ok=True)
+
+    return({
+        "DATA_DIR": DATA_DIR
+        ,"OUTPUT_DIR": OUTPUT_DIR
+    })
+    
+def get_limited_streaming_as_staging(end_time_column):
+    # function to query the limited data
+    # requires "FROM data" to reference limited data and not the entire history
+    return(
+        duckdb.sql(f'''
+        SELECT {end_time_column} AS end_time
+            ,artistName AS artist_name
+            ,trackName AS track_name
+            ,ROUND(msPlayed / {ms_to_mins},2) AS mins_played
+            ,ROUND(mins_played / {mins_to_hrs},2) AS hrs_played
+            ,CASE
+                WHEN LENGTH(CAST(month(end_time) AS VARCHAR))=1 THEN concat(year(end_time), '-0', month(end_time))
+                ELSE concat(year(end_time), '-',  CAST(month(end_time) AS VARCHAR))
+            END AS end_time_yyyy_mm
+            ,date(endTime) AS end_time_date
+        FROM data
+        WHERE year({end_time_column}) = 2025
+    '''
+        )
+)
--- spotify_analysis.py
+++ spotify_analysis.py
@@ -9,171 +9,56 @@ import duckdb
 my_utils.setup_logging("spotify_analysis.log")
 logging.info("Starting Spotify Analysis Pipeline")
 
-# Setting paths up
-json_subpath = "spotify_account_data_dec_2025"
+#%%
+# Set stuff up to consider querying from limited listening history or full listening history
+# 0 index is limited listening history (Dec 2024 - Dec 2025)
+# 1 index is full listening history up until Christmas Day 2025
+# 2 index is Solomon's full listening history up until Christmas Day 2025
+
+# where the JSONs are located
+json_subpath = ["spotify_account_data_dec_2025"
+                ,"spotify_extended_streaming_history"
+                ,"solomon_spotify_extended_streaming_history"
+    ]
+
+# what to call the combined file
+combined_file_name = ["dec_2025_streaming_history_combined.csv"
+                      ,"spotify_extended_streaming_history_combined.csv"
+                      ,"solomon_spotify_extended_streaming_history_combined.csv"
+    ]
+
+pattern = ["StreamingHistory_music*.json"
+    ,"Streaming_History_Audio*"
+    ]
+
+end_time_col = ["endTime"
+                ,"ts"
+    ]
+
+# where to put the combined file (I want to put them in the same place)
 csv_subpath = "streaming_history_data"
-combined_file_name = "streaming_history_combined.csv"
 
-DATA_DIR = Path(os.path.join(os.path.dirname(__file__), json_subpath)) 
+# which file from the above lists do I want to query?
+# for the file_index_level
+# indexes json_subpath and combined_file_name
+    # 0 = my subset of data
+    # 1 = my full listening history
+    # 2 = Solomon's full listening history
+# for the pattern_and_end_col_index_level
+# indexes pattern and end_time_col
+    # 0 = subset of data
+    # 1 = full listening history
+file_index_level = 2
+pattern_and_end_col_index_level = 1
 
-OUTPUT_DIR = Path(os.path.join(os.path.dirname(__file__), csv_subpath))
-OUTPUT_DIR.mkdir(exist_ok=True)
+# Setting paths up
+DATA_DIR = my_utils.init_paths(json_subpath[file_index_level],csv_subpath)["DATA_DIR"]
 
-#%%
-my_utils.get_csvs(DATA_DIR, OUTPUT_DIR, csv_subpath)
-my_utils.make_combined_csv(OUTPUT_DIR, combined_file_name)
-
-#%%
-data = duckdb.read_csv(Path(os.path.join(OUTPUT_DIR,combined_file_name)))
-
-#%%
-# Clean up the house
-ms_to_mins = 60000
-mins_to_hrs = 60
-
-base = duckdb.sql('''
-        SELECT *
-        FROM data
-    '''
-)
-
-staging = duckdb.sql(f'''
-        SELECT endTime AS end_time
-            ,artistName AS artist_name
-            ,trackName AS track_name
-            ,ROUND(msPlayed / {ms_to_mins},2) AS mins_played
-            ,ROUND(mins_played / {mins_to_hrs},2) AS hrs_played
-            ,CASE
-                WHEN LENGTH(CAST(month(end_time) AS VARCHAR))=1 THEN concat(year(end_time), '-0', month(end_time))
-                ELSE concat(year(end_time), '-',  CAST(month(end_time) AS VARCHAR))
-            END AS end_time_yyyy_mm
-            ,date(endTime) AS end_time_date
-        FROM data
-    '''
-)
-
-streaming_history_2025 = duckdb.sql('''
-        SELECT *
-        FROM staging
-        WHERE YEAR(end_time) = 2025
-    '''
-)
+OUTPUT_DIR = my_utils.init_paths(json_subpath[file_index_level],csv_subpath)["OUTPUT_DIR"]
 
 #%%
-# Timeframe of 2025 data collected (which differs from the timeframe Wrapped pulls from)
-duckdb.sql(f'''
-        SELECT min(end_time), max(end_time)
-        FROM streaming_history_2025
-    '''
-)
-
-# %%
-# Top x artists
-x = 5
-duckdb.sql(f'''
-        SELECT artist_name, ROUND(SUM(mins_played),2) AS mins_played
-        FROM streaming_history_2025
-        GROUP BY artist_name
-        ORDER BY SUM(mins_played) DESC
-        LIMIT {x}
-    '''
-)
-
-# %%
-# Top y songs
-y = 20
-duckdb.sql(f'''
-            SELECT track_name, artist_name, COUNT(*) AS number_of_streams, ROUND(SUM(mins_played),2) AS mins_played
-            FROM streaming_history_2025
-            GROUP BY track_name, artist_name
-            ORDER BY COUNT(*) DESC
-            LIMIT {y}
-    '''
-)
-
-# %%
-# Streaming by month and grand total
-duckdb.sql(f'''
-           WITH monthly AS (
-            SELECT DISTINCT end_time_yyyy_mm
-                , ROUND(SUM(mins_played)/60,2) AS hrs_played
-            FROM streaming_history_2025
-            GROUP BY ALL
-            ORDER BY end_time_yyyy_mm
-        )
-        , total AS (
-            SELECT 'Grand total', ROUND(SUM(mins_played)/60,2) AS hrs_played
-            FROM streaming_history_2025
-        )
-        SELECT * FROM monthly
-        UNION ALL
-        SELECT * FROM total
-    '''
-)
+my_utils.get_csvs(DATA_DIR, OUTPUT_DIR, csv_subpath, pattern[pattern_and_end_col_index_level])
+my_utils.make_combined_csv(OUTPUT_DIR, combined_file_name[file_index_level], end_time_col[pattern_and_end_col_index_level],pattern[pattern_and_end_col_index_level])
 
 #%%
-# Average hours listened to per day
-duckdb.sql(f'''
-        SELECT ROUND(
-                (
-                SUM(hrs_played)
-                /
-                -- get the # of days in the time period by casting the interval as a string, parsing it for the #, then casting it as an integer
-                CAST(CAST(max(end_time)-min(end_time) AS VARCHAR)[1:4] AS INT)
-                )
-            ,2) AS daily_avg_hrs_played
-        FROM streaming_history_2025
-    '''
-)
-
-#%%
-# Top z listening days
-z = 5
-duckdb.sql(f'''
-        SELECT DISTINCT end_time_date, ROUND(SUM(hrs_played),2) AS total_hrs_played
-        FROM streaming_history_2025
-        GROUP BY end_time_date
-        ORDER BY SUM(hrs_played) DESC
-        LIMIT {z}
-    '''
-)
-
-# %%
-# Top a artists on my top listening day in terms of # of listens (not mins listened)
-a = 5
-duckdb.sql(f'''
-        SELECT artist_name, count(*)
-        FROM streaming_history_2025
-        WHERE end_time_date = (
-            SELECT DISTINCT end_time_date
-            FROM streaming_history_2025
-            GROUP BY end_time_date
-            ORDER BY SUM(hrs_played) DESC
-            LIMIT 1
-        )
-        GROUP BY artist_name
-        ORDER BY count(*) DESC
-        LIMIT {a}
-    '''
-)
-
-#%%
-# Top b songs on my top listening day in terms of # of listens (not mins listened)
-b = 5
-duckdb.sql(f'''
-        SELECT track_name, count(*)
-        FROM streaming_history_2025
-        WHERE end_time_date = (
-            SELECT DISTINCT end_time_date
-            FROM streaming_history_2025
-            GROUP BY end_time_date
-            ORDER BY SUM(hrs_played) DESC
-            LIMIT 1
-        )
-        GROUP BY track_name
-        ORDER BY count(*) DESC
-        LIMIT {b}
-    '''
-)
-
-# %%
+data = duckdb.read_csv(Path(os.path.join(OUTPUT_DIR,combined_file_name[file_index_level])))
